de6c12730c51c6c533daa2267adb3d51920b0772
admin <252097221@qq.com>
2021-11-16 17:14:08 +0800

    增加 postasn/postso未完成

--- WMSAPI.py
+++ WMSAPI.py
@@ -14,7 +14,7 @@ flask： web框架，通过flask提供的装饰器@server.route()将普通函数
 app = Flask(__name__)
 @app.route('/', methods=['get', 'post'])
 def index():
-    return "接口服务连通!"
+    return "接口服务连通!"+request.url
 #入库单接口
 @app.route('/postasn', methods=['get', 'post'])
 #出库单接口
@@ -22,31 +22,41 @@ def index():
 #产品档案接口
 @app.route('/postsku', methods=['get', 'post'])
 def login():
+    #获取访问URL分支，对应不同的数据处理逻辑
+    loc=request.url[request.url.rfind("/") + 1:request.url.find("?")]
     #客户列表，允许调用的客户
     userlist = ['XH']
     # 获取通过url请求传参的数据
     userid = request.values.get('userid')
     # 获取url请求传的密码，明文
     pwd = request.values.get('pwd')
+    # 获取post的json数据
+    data = request.data.decode()
     ##这里是业务，看你怎么写了，想写什么写什么
     # 判断用户名、密码都不为空，如果不传用户名、密码则username和pwd为None
-    if userid and pwd:
-        if userid in userlist and pwd == 'test1234':
-            data = request.data.decode()
-            data=data.replace('\r\n','')
-            # 调用putjson写入数据库
-            putjson.putjson(data,"postsku")
-            resu ={'code': 000, 'message': str(data)} # 将字典转换为json串, json是字符串
-            return json.dumps(resu, ensure_ascii=False)
-        else:
-            resu = {'code': 998, 'message': '账号密码错误'}
-            return json.dumps(resu, ensure_ascii=False)
-    else:
-        resu = {'code': 999, 'message': '参数不完整请提供接口URL反馈IT'}
+    if userid is None or pwd is None:
+        resu = {'code': 998, 'message': '账号密码不能为空'}
         return json.dumps(resu, ensure_ascii=False)
+    if len(data)==0:
+        resu = {'code': 999, 'message': ' POST data内空不能为空'}
+        return json.dumps(resu, ensure_ascii=False)
+    if userid not in userlist:
+        resu = {'code': 999, 'message': '客户ID不正确'}
+        return json.dumps(resu, ensure_ascii=False)
+    if pwd != 'test1234':
+        resu = {'code': 999, 'message':'密钥不正确'}
+        return json.dumps(resu, ensure_ascii=False)
+
+    data=data.replace('\r\n','')
+    # 调用putjson写入数据库
+    putjson.postsku(loc,data)
+    resu ={'code': 000, 'message': str(data)} # 将字典转换为json串, json是字符串
+    return json.dumps(resu, ensure_ascii=False)
+
+
 
 
 # host="0.0.0.0" 代表谁都可以访问，可以加nginx处理
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=58080, debug=True,threaded=True)
+    app.run(host="0.0.0.0", port=8080, debug=True,threaded=True)
     ###指定端口、host设为0.0.0.0代表不管几个网卡，任何ip都可以访问,threaded=True同时执行多个路由请求,processes=线程数
--- test1.py
+++ test1.py
@@ -1,36 +1,4 @@
-import time
-import datetime
-import cx_Oracle
-import requests
-import json
-#连接数据库
-conn = cx_Oracle.connect('WMS_USER/WMS_USER@47.100.131.95:1521/orcl')
-#定义2个游标
-curs = conn.cursor()
-curs1 = conn.cursor()
-#sql语句，带参数的执行要给入参
-sql = "select id,url,content from httppost where status='0' and appid='WXALERT'"
-update = "UPDATE httppost SET message=:1,status=:2,sendtime=to_date(:3,'yyyy-mm-dd HH24:MI:SS') WHERE id=:4 "
-#执行查询sql，查询出来的数据按行循环
-curs.execute(sql)
-for table in curs:
-    print(table)
-    #取行中列顺序对应的值
-    id=table[0]
-    url = table[1]
-    c = table[2]
-    #组织post请求发送的内容
-    content =c #' {"msgtype": "text","text": {"content":"' + c + '","mentioned_list":["@all"]}}'
-    print(content)
-    headers={"Content-Type": "application/json"}
-    #调用http post请求，中文要加encode
-    r = requests.post(url=url,headers=headers,data=content.encode('utf-8'))
-    print(r.text)
-    #带参数的update,提前组织好参数，日期格式传str，在sql中转date
-    info=(r.text,'1',datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),id)
-    curs1.execute(update,info)
-#提交，关闭连接
-conn.commit()
-curs.close()
-conn.close()
-time.sleep(5)
+str="http://172.20.115.150:8080/postsku?1=1"
+post=str.rfind("/")
+print(str)
+print(str[str.rfind("/")+1:str.find("?")])
